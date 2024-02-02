acae6a8f36bb7923e48177b055f233fdc590beaf
yuseirf <yusei.tanaka2112325@icloud.com>
2024-02-02 09:40:22 -0300

    Add pdf ocr funtion

---
+++ pdf_ocr.py
@@ -0,0 +1,36 @@
+# from: https://qiita.com/Hagian/items/f508d5996e05f4497557
+
+from pathlib import Path
+from pdf2image import convert_from_path
+import pyocr
+import pyocr.builders
+import sys
+
+# 使用可能なOCRツールを探す
+tools = pyocr.get_available_tools()
+tool = tools[0]
+if len(tools) == 0:
+    print("OCR tool is not found.")
+    sys.exit(1)
+lan = "eng"
+
+# 抽出したいファイル名を指定
+f_name = "test"
+
+# パスを設定
+pdf_path = Path(f"{f_name}.pdf")
+txt_path = Path(f"{f_name}.txt")
+
+# PDFから画像ファイルに変換
+pages = convert_from_path(str(pdf_path), 300)
+
+print("converted!")
+
+txt = ""
+for i, page in enumerate(pages):
+    txt = txt + tool.image_to_string(page, lang=lan, builder=pyocr.builders.TextBuilder(tesseract_layout=6))
+
+# テキストファイルに書き出し
+with open(txt_path, mode="w") as f:
+    f.write(txt)
+print("Process completed!")
